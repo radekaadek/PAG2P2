ea1139a27317aa2f37c96e22a91008cfa9e9a276
magzsko <maksskorupowski@gmail.com>
2025-01-21 20:26:08 +0100

    mongodb get by polygon

--- mongo.py
+++ mongo.py
@@ -1,6 +1,7 @@
 import pymongo
 import json
 import geopandas as gpd
+from shapely.geometry import mapping
 
 def mongo_init(gj_path, mongo_adr):
     #Reading geojson with data
@@ -20,8 +21,30 @@ def mongo_init(gj_path, mongo_adr):
     column.create_index([("geometry", "2dsphere")])
     return connection, database, column
 
+def mongo_get_by_polygon(column, poly):
+    query = {
+        "geometry": {
+            "$geoWithin": {
+                "$geometry": poly
+            }
+        }
+    }
+    features = list(column.find(query))
+    return features
+
 if __name__ == '__main__':
     geojson_path = 'Projekt-blok-2/Dane/data.geojson'
     mongo_address = "mongodb://localhost:27017/"
+    woj_geojson_path = 'Projekt-blok-2/Dane/woj.geojson'
+
+
+    woj_gdf = gpd.read_file(woj_geojson_path)
+    first_polygon = woj_gdf.iloc[0].geometry
+    polygon_gdf = gpd.GeoDataFrame(geometry=[first_polygon], crs="EPSG:4326")
+    polygon = mapping(first_polygon)
+
     con, db, col = mongo_init(geojson_path, mongo_address)
+    features_by_polygon = mongo_get_by_polygon(col, polygon)
     con.close()
+
+    print(len(features_by_polygon))
