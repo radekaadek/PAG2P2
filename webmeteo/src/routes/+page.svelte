5d53a5c4db39c6d837e4fdcb7052a10fde20532a
radekaadek <radekaadek@gmail.com>
2025-01-21 17:01:21 +0100

    formatting

--- webmeteo/src/routes/+page.svelte
+++ webmeteo/src/routes/+page.svelte
@@ -1,84 +1,86 @@
-{#if loading}
-	<div id='loading'>Loading...</div>
-{/if}
-
-
 <script lang="ts">
-import L from "leaflet";
+  import L from "leaflet";
 
-const mapdiv = document.createElement("div");
-// set state to hidden
-document.body.appendChild(mapdiv);
-mapdiv.id = "map";
-mapdiv.style.height = "0vh";
-mapdiv.style.width = "0vw";
+  const mapdiv = document.createElement("div");
+  // set state to hidden
+  document.body.appendChild(mapdiv);
+  mapdiv.id = "map";
+  mapdiv.style.height = "0vh";
+  mapdiv.style.width = "0vw";
 
-let loading = $state(true);
+  let loading = $state(true);
 
-const map = L.map("map").setView([52, 21], 7);
+  const map = L.map("map").setView([52, 21], 7);
 
-L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-	maxZoom: 19,
-	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
-
-// request voivodeships from server
-const base_url = "http://0.0.0.0:8000";
-const voivodeships_url = `${base_url}/voivodeships`;
-const powiats_url = `${base_url}/powiats`;
-
-const osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
     maxZoom: 19,
-    attribution: '© OpenStreetMap'
-});
-const osmHOT = L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+    attribution:
+      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  }).addTo(map);
+
+  // request voivodeships from server
+  const base_url = "http://0.0.0.0:8000";
+  const voivodeships_url = `${base_url}/voivodeships`;
+  const powiats_url = `${base_url}/powiats`;
+
+  const osm = L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
     maxZoom: 19,
-    attribution: '© OpenStreetMap contributors, Tiles style by Humanitarian OpenStreetMap Team hosted by OpenStreetMap France'});
-const baseMaps = {
-    "OpenStreetMap": osm,
-    "OpenStreetMap.HOT": osmHOT
-};
+    attribution: "© OpenStreetMap",
+  });
+  const osmHOT = L.tileLayer(
+    "https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png",
+    {
+      maxZoom: 19,
+      attribution:
+        "© OpenStreetMap contributors, Tiles style by Humanitarian OpenStreetMap Team hosted by OpenStreetMap France",
+    },
+  );
+  const baseMaps = {
+    OpenStreetMap: osm,
+    "OpenStreetMap.HOT": osmHOT,
+  };
 
-const layerControl = L.control.layers(baseMaps).addTo(map);
+  const layerControl = L.control.layers(baseMaps).addTo(map);
 
-fetch(voivodeships_url, {
-	method: "GET",
-})
-	.then((response) => response.json())
-	.then((data) => {
-    const d = L.geoJSON(data, { style: { color: 'red', weight: 4 } });
-    // Add the layer to the map directly
-    d.addTo(map); // 'map' is your Leaflet map instance
+  fetch(voivodeships_url, {
+    method: "GET",
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      const d = L.geoJSON(data, { style: { color: "red", weight: 4 } });
+      // Add the layer to the map directly
+      d.addTo(map); // 'map' is your Leaflet map instance
 
-    // Add the layer to the layer control
-    layerControl.addOverlay(d, "Voivodeships");
+      // Add the layer to the layer control
+      layerControl.addOverlay(d, "Voivodeships");
 
-    // set loading to false
-    loading = false;
-    mapdiv.style.height = "100vh";
-    mapdiv.style.width = "100vw";
-    
-	});
+      // set loading to false
+      loading = false;
+      mapdiv.style.height = "100vh";
+      mapdiv.style.width = "100vw";
+    });
 
-fetch(powiats_url, {
-	method: "GET",
-})
-	.then((response) => response.json())
-	.then((data) => {
-    const d = L.geoJSON(data, { style: { color: 'blue', weight: 4 } });
-    layerControl.addOverlay(d, "Powiaty");
-    
-	});
-
+  fetch(powiats_url, {
+    method: "GET",
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      const d = L.geoJSON(data, { style: { color: "blue", weight: 4 } });
+      layerControl.addOverlay(d, "Powiaty");
+    });
 </script>
 
+{#if loading}
+  <div id="loading">Loading...</div>
+{/if}
+
 <style>
-#loading {
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  color: red;
-  font-size: 2em;
-}
+  #loading {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    color: red;
+    font-size: 2em;
+  }
 </style>
