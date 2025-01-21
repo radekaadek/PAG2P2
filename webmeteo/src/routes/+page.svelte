fcdbd1c9d628b95064150fb5b54529a263f2dee9
radekaadek <radekaadek@gmail.com>
2025-01-21 16:42:00 +0100

    layer control

--- webmeteo/src/routes/+page.svelte
+++ webmeteo/src/routes/+page.svelte
@@ -1,11 +1,9 @@
 <script lang="ts">
 import L from "leaflet";
 
-// add div with id="map" to your svelte component
 const mapdiv = document.createElement("div");
 document.body.appendChild(mapdiv);
 mapdiv.id = "map";
-// add height:180px to your svelte component
 mapdiv.style.height = "100vh";
 mapdiv.style.width = "100vw";
 
@@ -18,15 +16,45 @@ L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
 
 // request voivodeships from server
 const base_url = "http://0.0.0.0:8000";
-const url = `${base_url}/voivodeships`;
+const voivodeships_url = `${base_url}/voivodeships`;
+const powiats_url = `${base_url}/powiats`;
 
-fetch(url, {
+const osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '© OpenStreetMap'
+});
+const osmHOT = L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '© OpenStreetMap contributors, Tiles style by Humanitarian OpenStreetMap Team hosted by OpenStreetMap France'});
+const baseMaps = {
+    "OpenStreetMap": osm,
+    "OpenStreetMap.HOT": osmHOT
+};
+
+const layerControl = L.control.layers(baseMaps).addTo(map);
+
+fetch(voivodeships_url, {
 	method: "GET",
 })
 	.then((response) => response.json())
 	.then((data) => {
-//{ style: { color: 'red', weight: 4 }
-    L.geoJSON(data, { style: { color: 'red', weight: 4 } }).addTo(map);
+    const d = L.geoJSON(data, { style: { color: 'red', weight: 4 } });
+    // Add the layer to the map directly
+    d.addTo(map); // 'map' is your Leaflet map instance
+
+    // Add the layer to the layer control
+    layerControl.addOverlay(d, "Voivodeships");
+    
+	});
+
+fetch(powiats_url, {
+	method: "GET",
+})
+	.then((response) => response.json())
+	.then((data) => {
+    const d = L.geoJSON(data, { style: { color: 'blue', weight: 4 } });
+    layerControl.addOverlay(d, "Powiaty");
+    
 	});
 
 </script>
